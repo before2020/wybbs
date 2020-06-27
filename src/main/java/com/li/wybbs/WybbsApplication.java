1a5089f3281f9ca68d27aef15eba012fbf0ca949
wanyi-li <wanyihere@163.com>
2020-06-27 11:17:04 +0800

    first commit

---
+++ src/main/java/com/li/wybbs/WybbsApplication.java
@@ -0,0 +1,11 @@
+package com.li.wybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WybbsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WybbsApplication.class, args);
+    }
+}
